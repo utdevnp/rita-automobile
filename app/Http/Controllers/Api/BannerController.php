98a612fd6f4b42d2b4ecea651f59bc5351d8cb9f
Utshab Luitel <utdevnp@gmail.com>
2021-04-11 16:32:21 +0545

    fix banner and setting api

--- app/Http/Controllers/Api/BannerController.php
+++ app/Http/Controllers/Api/BannerController.php
@@ -4,12 +4,28 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Resources\BannerCollection;
 use App\Models\Banner;
-
+use App\Http\Controllers\Api\ResponseController;
 class BannerController extends Controller
 {
+    public function __construct(ResponseController $response){
+        $this->response = $response;
+    }
+
+
 
     public function index()
     {
-        return new BannerCollection(Banner::all());
+        $Banners =   new BannerCollection(Banner::all());
+
+       
+
+        if(! $Banners){
+            return $this->response->error([
+                'message'=>"Banner not added yet",
+                'data'=>null
+            ]);
+        }
+        return $Banners;
+
     }
 }
